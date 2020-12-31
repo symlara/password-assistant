07f3a02854da90268760a2f1a1faa33d657aabad
symlar <40181569+symlara@users.noreply.github.com>
2020-12-30 22:21:36 -0600

    nothing new added yet

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("What's your password criteria?") });
+var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("What would you like your password to be?") });
 
 // Write password to the #password input
 function writePassword() {
