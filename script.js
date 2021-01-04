2ba55b38aab48dbb80e801600d79a9d457199349
symlar <40181569+symlara@users.noreply.github.com>
2021-01-04 17:15:58 -0600

    added event listeners

--- script.js
+++ script.js
@@ -2,16 +2,31 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("What would you like your password to be?") });
+var generateBtn = document.querySelector("#generate");
+
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(i);
   var passwordText = document.querySelector("#password");
 
+  
+
   passwordText.value = password;
+  
+  
 
 }
+  
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("What would you like the length of the password to be?")});
+
+var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("Must be at least 8 characters and no more than 128 characters")})
+
+var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("What kind of character types are needed in the password?")});
+
+var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("lowercase, uppercase, numeric, and/or special characters")});
