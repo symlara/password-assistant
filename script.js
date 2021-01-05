34f327ef4d93c4e8f6a8160fcf90e13d24535370
symlar <40181569+symlara@users.noreply.github.com>
2021-01-04 21:30:45 -0600

    added random characters to function in js file

--- script.js
+++ script.js
@@ -7,21 +7,20 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword(i);
+function writePassword(length) {
+  var password = "Abcdefgh00005678910!!";
   var passwordText = document.querySelector("#password");
-
   
-
-  passwordText.value = password;
-  
-  
+  passwordText.value =  password;
 
 }
   
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
+
+
 
 var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("What would you like the length of the password to be?")});
 
@@ -30,3 +29,4 @@ var generateBtn = document.querySelector("#generate").addEventListener("click", 
 var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("What kind of character types are needed in the password?")});
 
 var generateBtn = document.querySelector("#generate").addEventListener("click", function() { alert("lowercase, uppercase, numeric, and/or special characters")});
+
